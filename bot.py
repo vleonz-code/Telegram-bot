f5ac18ae2a16a4919bd4e2ac48cd569aee2698aa
vleonz-code <vleonz@icloud.com>
2026-07-08 09:13:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -226,7 +226,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Silently ignore banned users
 # Notify banned users
 if user_id in read_blacklist():
-    await update.message.reply_text(
+    await update.effective_message.reply_text(
         "🚫 Akses Anda telah dibatasi."
     )
     return
