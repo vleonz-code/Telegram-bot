c49353a91b22c55b6804aacb10cc34348c9daf95
vleonz-code <vleonz@icloud.com>
2026-07-10 16:09:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,10 @@ def save_vip_packages(data):
     with open(VIP_PACKAGES_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+    github_commit_file(
+        VIP_PACKAGES_FILE,
+        "Update vip_packages.json"
+    )
 # ==========================
 
 # SETTINGS 
