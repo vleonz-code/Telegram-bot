46938d8a87ba0b03959d2d5ec8f332ebc8373fe8
vleonz-code <vleonz@icloud.com>
2026-07-12 18:13:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -691,13 +691,6 @@ async def bayar1_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
 
-    #if is_payment_locked(query.from_user.id):
-        #await query.answer(
-   #         "⏳ Anda masih memiliki transaksi yang belum selesai.",
-  #          show_alert=True
-   #     )
-        #return
-
     package_id = int(query.data.split("_")[1])
     package = get_package(package_id)
 
