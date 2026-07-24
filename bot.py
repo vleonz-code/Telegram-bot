5ca680e663741dadf8891616ae6a1100f7aabea0
vleonz-code <vleonz@icloud.com>
2026-07-25 01:06:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1872,7 +1872,7 @@ async def delete_messages_after_delay(
                 text=(
                     "⏰ Masa Preview sudah selesai.\n\n"
                     "Koleksi selengkapnya ada di grup VIP.\n\n"
-                    "Chat Admin: 👉🏻 @BocilVIP89"
+                    "👑 Chat Admin: @BocilVIP89"
                 ),
                 reply_markup=keyboard
             )
