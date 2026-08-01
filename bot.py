84906b4d687b2cdfd3a0239bbd86f68cb0b32837
vleonz-code <vleonz@icloud.com>
2026-08-01 20:05:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4172,9 +4172,7 @@ async def filemgr_view_callback(update: Update, context: ContextTypes.DEFAULT_TY
         "👁 <b>FILE PREVIEW</b>\n\n"
         f"📄 <b>{display_name}</b> • JSON\n"
         f"📏 {size_str} • 📋 {records} Records\n\n"
-        f"<pre>{html.escape(preview_text)}</pre>\n\n"
-        "⚠️ Sample data pertama.\n"
-        "Gunakan Edit untuk melihat isi lengkap."
+        f"<pre>{html.escape(preview_text)}</pre>"
     )
 
     try:
