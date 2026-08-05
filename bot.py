8df5a266e417ade6237f75a2a96bafecc4ed0e2d
vleonz-code <vleonz@icloud.com>
2026-08-05 16:28:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -999,7 +999,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             chat_id=update.effective_chat.id,
             text=(
                 "📍 Permintaan ulang belum tersedia.\n\n"
-                "🔐 Koleksi rare eksklusif, "
+                "🔐 Sike Chill Koleksi rare eksklusif, "
                 "update setiap hari, dan berbagai genre ochil. ୨୧\n\n"
             ),
             reply_markup=keyboard
