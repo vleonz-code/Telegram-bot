83cd1751c83fdecd03329e4d572654ea35b95b4c
vleonz-code <vleonz@icloud.com>
2026-08-08 12:55:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5872,6 +5872,7 @@ async def adminvip(update: Update, context: ContextTypes.DEFAULT_TYPE):
     settings = read_settings()
 
     admin_panel_text = (
+        "<b>👑 ADMIN VIP PANEL</b>\n"
         "<pre>"
 
         f"👥 Users       : {len(read_user_registry())}\n"
