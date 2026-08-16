5665e4663154a65de9e62d4ca18b00e9efc20a4f
vleonz-code <vleonz@icloud.com>
2026-08-16 10:00:59 +0700

    Auto-sync changes from VPS

--- bot.py
+++ bot.py
@@ -1592,6 +1592,7 @@ def build_vip_package_text(package):
             "",
             "╭" + "─" * box_width + "╮",
             "<b>      " + intro_safe + "      </b>",
+            "",
         ])
 
     if benefits:
